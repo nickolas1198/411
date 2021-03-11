39b078822c1159bb326d92c4f47c22abd53f596a
Blaine <blaine.prestenbach@selu.edu>
2021-03-11 13:48:36 -0600

    Feature: Clear Console Output window click "Run" button
    Empties the <AceEditorConsoleOutput> "value" prop when the Run button is clicked

--- 411Project.Web/client-app/src/components/Navbar/RunButton.tsx
+++ 411Project.Web/client-app/src/components/Navbar/RunButton.tsx
@@ -12,10 +12,18 @@ export type RunButtonInfo = Judge0Dto & {
 
 const RunButton = (props: RunButtonInfo) => {
   const handleSubmit = async (dto: Judge0Dto) => {
+    // Activate the LoadingOverlay component
     props.setLoading(true);
+
+    // Empty the AceEditorConsoleOutput "value" prop
+    props.setStdout("");
+    props.setStderr("");
+    props.setCompileOutput("");
+
     let res = await Judge0ApiCall(dto);
     props.setLoading(false);
 
+    // Display the response in AceEditorConsoleOutput
     props.setStdout(res.data.stdout);
     props.setStderr(res.data.stderr);
     props.setCompileOutput(res.data.compile_output);
