ff6f7c6f7f5ec7fd69ec924424f69a21fef3b466
Nickolas <nickolas1198@hotmail.com>
2021-03-25 21:45:25 -0500

    bugfix
    Fixed a small bug that prevented the program from compiling

--- 411Project.Web/client-app/src/components/Navbar/NavbarElements.js
+++ 411Project.Web/client-app/src/components/Navbar/NavbarElements.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> basic-frontend
 import { NavLink as Link } from "react-router-dom";
 import styled from "styled-components";
 
@@ -11,38 +7,10 @@ export const NavLink = styled(Link)`
   display: flex;
   align-items: center;
   text-decoration: none;
-  padding: 1rem 1rem;
+  padding:1rem 1rem;
   height: 100%;
   cursor: pointer;
   &:active {
     color: #fff;
   }
 `;
-<<<<<<< HEAD
-
-export const NavBtn = styled.nav`
-  display: flex;
-  align-items: center;
-  margin-right: 24px;
-`;
-
-export const NavBtnLink = styled(Link)`
-  border-radius: 4px;
-  background: linear-gradient(to right, #67b26b, #4ca2cb) !important;
-  padding: 10px 22px;
-  color: #fff;
-  outline: none;
-  border: none;
-  cursor: pointer;
-  transition: all 0.2s ease-in-out;
-  text-decoration: none;
-
-  margin-left: 24px;
-  &:hover {
-    transition: all 0.2s ease-in-out;
-    background: #fff;
-    color: #010606;
-  }
-`;
-=======
->>>>>>> basic-frontend
